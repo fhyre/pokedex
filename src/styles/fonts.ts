3ec58d8484e79d36fb6e9430a1c2d0fda8a9709f
Henry <vividnight@protonmail.com>
2024-03-10 14:27:57 -0400

    style: rename breakpoints clean font file

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,16 +1,7 @@
-import { Barlow, Inter, Nunito, Titillium_Web } from 'next/font/google';
+import { Inter, Titillium_Web } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
-});
-
-export const nunito = Nunito({
-  subsets: ['latin'],
-});
-
-export const barlow = Barlow({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const titilliumWeb = Titillium_Web({
