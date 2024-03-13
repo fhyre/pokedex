bb027197f90e691ea659a2241762db6cfb1eb17f
Henry <vividnight@protonmail.com>
2024-03-13 00:32:55 -0400

    remove: unused components removed

--- src/features/ui/card/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './card.module.css';
-
-export function Card({ name }) {
-  return (
-    <section className={`${styles.container}`}>
-      <h2>{name}</h2>
-    </section>
-  );
-}
